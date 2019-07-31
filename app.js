ae3658a8c2afbea5c2953dfcc0e50fe57ebaa384
mcshakes <efmccarthy26@gmail.com>
2019-07-31 10:04:16 -0600

    More postbuild issues

--- app.js
+++ app.js
@@ -19,7 +19,12 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 
 const port = process.env.PORT || 3001
