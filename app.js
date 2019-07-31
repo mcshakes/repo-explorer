237a043269475a504254cd0c8dd92d3c3a786a8a
mcshakes <efmccarthy26@gmail.com>
2019-07-31 12:42:28 -0600

    Added concurrently to simplify the dev process to one terminal window; Removed the get * filepath line since I don't think I needed it.

--- app.js
+++ app.js
@@ -22,9 +22,9 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// });
 
 
 const port = process.env.PORT || 3001
