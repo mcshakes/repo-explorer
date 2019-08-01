15137b9250e2dd35a57ac683fd164dfb7b2fe9e4
mcshakes <efmccarthy26@gmail.com>
2019-08-01 15:54:49 -0600

    Fixed up card views; still not too satisfied with how they look. I can see myself sinking a lot of time in this CSS segment

--- client/src/components/Repositories/components/RepoItem.js
+++ client/src/components/Repositories/components/RepoItem.js
@@ -3,16 +3,15 @@ import "./RepoItem.css";
 
 const RepoItem = props => {
     return <li className="repo__list-item">
-        <div>
+        <div className="repo__list-item-info">
             <h3>{props.name}</h3>
             <h4>{props.description}</h4>
-        </div>
-        <div>
             <p>WRITTEN IN: {props.language}</p>
             <p>STARS: {props.star}</p>
         </div>
         <div className="repo__list-item-image">
             <img src={props.owner.avatar_url} alt={props.owner.login} />
+            <h2>{props.owner.login}</h2>
         </div>
     </li>
 	
